f7024a173014dfaed07efc6f78147edca9f5a6e1
Mandar Patil <mandar.patil@willistowerswatson.com>
2022-04-19 15:18:00 +0530

    final checkin

--- src/Services/Discount/Discount.API/Controllers/DiscountController.cs
+++ src/Services/Discount/Discount.API/Controllers/DiscountController.cs
@@ -10,7 +10,7 @@ using System.Threading.Tasks;
 
 namespace Discount.API.Controllers
 {
-    [Route("api/[controller]")]
+    [Route("api/v1/[controller]")]
     [ApiController]
     public class DiscountController : ControllerBase
     {
